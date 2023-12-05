1ecd3bca4b1cd195fb8a41a56c1fe48bcd68bdfa
RongDu7 <151746090+RongDu7@users.noreply.github.com>
2023-12-05 21:59:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,64 @@
+require('dotenv').config()
 var express = require("express")
 var app = express()
+var cors = require("cors")
+
+let projectCollection;
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://doridu077:oQNuFuBZBjfmJvA7@sit725-2023-t2-prac4.1shdhdg.mongodb.net/?retryWrites=true&w=majority"
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
+    // client.db("admin").collection("pets");  
+    
+    //client.db().createCollection('userlist')
+    console.log("projectCollection created!")
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.use(express.static(__dirname+'/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-const MongoClient = require('mongodb').MongoClient;
-let projectCollection;
-
-// Database Connection
-const uri = "mongodb+srv://doridu077:oQNuFuBZBjfmJvA7@sit725-2023-t2-prac4.1shdhdg.mongodb.net"
-const client = new MongoClient(uri,{ useNewUrlParser: true })
+app.use(cors())
 
 const createColllection = (collectionName) => {
-    client.connect((err,db) => {
-        projectCollection = client.db().collection(collectionName);
-        if(!err) {
-            console.log('MongoDB Connected')
-        }
-        else {
-            console.log("DB Error: ", err);
-            process.exit(1);
-        }
-    })
+    client.connect();
+    projectCollection = client.db().createCollection(collectionName);
+
+//    client.connect((err,db) => {
+//        console.log('Start creating projectColletion')
+//      projectCollection = client.db().createCollection(collectionName);
+//        console.log('Start MongoDB client')
+//      if(!err) {
+//            console.log('MongoDB Connected')
+//        }
+//        else {
+//            console.log("DB Error: ", err);
+//            process.exit(1);
+//        }
+//      console.log('Finish MongoDB client')
+//    })
 }
 
 const insertProjects = (project,callback) => {
@@ -32,8 +69,20 @@ const getProjects = (callback) => {
     projectCollection.find({}).toArray(callback);
 }
 
-
-
+const cardList = [
+    {
+        title: "Kitten 2",
+        image: "images/kitten-2.jpg",
+        link: "About Kitten 2",
+        description: "Demo desciption about kitten 2"
+    },
+    {
+        title: "Kitten 3",
+        image: "images/kitten-3.jpg",
+        link: "About Kitten 3",
+        description: "Demo desciption about kitten 3"
+    }
+]
 app.get('/api/projects',(req,res) => {
     getProjects((err,result) => {
         if(err) {
@@ -45,7 +94,7 @@ app.get('/api/projects',(req,res) => {
     })
 })
 
-app.post('/api/projects',(req,res) => {
+app.post('/api/addprojects',(req,res) => {
     console.log("New Project added", req.body)
     var newProject = req.body;
     insertProjects(newProject,(err,result) => {
@@ -57,21 +106,23 @@ app.post('/api/projects',(req,res) => {
         }
     })
 })
+const addNumbers = (number1, number2) => {
+    var num1 = parseInt(number1)
+    var num2 = parseInt(number2)
+    var result = num1 + num2;
+    return result;
+}
 
-
-const cardList = [ {
-    title: "Kitten 2",
-    image: "images/kitten-2.jpg",
-    link: "About Kitten 2",
-    desciption: "Demo desciption about kitten 2"
-    }, {
-    title: "Kitten 3",
-    image: "images/kitten-3.jpg",
-    link: "About Kitten 3",
-    desciption: "Demo desciption about kitten 3"
-}]
+app.get("/addTwoNumbers",(req,res) => {
+    var number1 = req.query.number1;
+    var number2 = req.query.number2;
+    var result = addNumbers(number1,number2)
+    res.json({statusCode: 200, data: result, message:"Success"})
+})
 
 var port = process.env.port || 3000;
+
 app.listen(port,()=>{
-console.log("App listening to: "+port)
+    console.log("App running at http://localhost:"+port)
+    createColllection("pets")
 })
