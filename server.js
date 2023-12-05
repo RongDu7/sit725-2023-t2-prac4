2b60a06e4227819c2e5bd898b0eb5378728848aa
RongDu7 <151746090+RongDu7@users.noreply.github.com>
2023-12-05 19:54:34 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,61 @@ app.use(express.static(__dirname+'/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const MongoClient = require('mongodb').MongoClient;
+let projectCollection;
+
+// Database Connection
+const uri = "mongodb+srv://doridu077:oQNuFuBZBjfmJvA7@sit725-2023-t2-prac4.1shdhdg.mongodb.net"
+const client = new MongoClient(uri,{ useNewUrlParser: true })
+
+const createColllection = (collectionName) => {
+    client.connect((err,db) => {
+        projectCollection = client.db().collection(collectionName);
+        if(!err) {
+            console.log('MongoDB Connected')
+        }
+        else {
+            console.log("DB Error: ", err);
+            process.exit(1);
+        }
+    })
+}
+
+const insertProjects = (project,callback) => {
+    projectCollection.insert(project,callback);
+}
+
+const getProjects = (callback) => {
+    projectCollection.find({}).toArray(callback);
+}
+
+
+
+app.get('/api/projects',(req,res) => {
+    getProjects((err,result) => {
+        if(err) {
+            res.json({statusCode: 400, message: err})
+        }
+        else {
+            res.json({statusCode: 200, message:"Success", data: result})
+        }
+    })
+})
+
+app.post('/api/projects',(req,res) => {
+    console.log("New Project added", req.body)
+    var newProject = req.body;
+    insertProjects(newProject,(err,result) => {
+        if(err) {
+            res.json({statusCode: 400, message: err})
+        }
+        else {
+            res.json({statusCode: 200, message:"Project Successfully added", data: result})
+        }
+    })
+})
+
+
 const cardList = [ {
     title: "Kitten 2",
     image: "images/kitten-2.jpg",
@@ -15,10 +70,6 @@ const cardList = [ {
     link: "About Kitten 3",
     desciption: "Demo desciption about kitten 3"
 }]
-
-app.get('/api/projects',(req,res) => {
-    res.json({statusCode: 200, data: cardList, message:"Success"})
-})
 
 var port = process.env.port || 3000;
 app.listen(port,()=>{
